9c486ff0e14ac11ccc781fa33f5d7a26ae83e301
Marwa <marwameheddien2000@gmail.com>
2022-09-08 09:54:35 +0300

    seventh project

---
+++ script.js
@@ -0,0 +1,7 @@
+const ursurprise=document.getElementById("birthday")
+function ursurprise(){
+    ursurprise.className="hey"
+}
+function goback(){
+    document.getElementById('birthday').className='';
+}
